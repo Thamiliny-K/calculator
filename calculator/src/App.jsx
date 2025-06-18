48fd1495cdee383e4c72692b445d4f017ba0181f
Thamiliny-K. <thamiliny09@gmail.com.>
2025-06-18 10:01:58 +0530

    my update calculator app

---
+++ calculator/src/App.jsx
@@ -0,0 +1,12 @@
+
+
+function App() {
+  
+  return (
+    <>
+    <h1 className="text-3xl text-amber-900">hi</h1>
+    </>
+  )
+}
+
+export default App
--- calculator/src/Cal.jsx
+++ calculator/src/Cal.jsx
@@ -2,7 +2,45 @@ import React from 'react'
 
 function Cal() {
   return (
-    <div>Calculator</div>
+    <>
+    <div className='w-full h-screen bg-red-900 flex justify-center items-center'>
+        <div className='bg-[#171314] inset-shadow-sm inset-shadow-zinc-500'>
+
+          <div className='w-full p-8 bg-[#171314] inset-shadow-sm inset-shadow-zinc-500'>
+          <input type='text'/>
+          </div>
+
+          <div className='grid p-4 grid-cols-4 gap-6'>
+
+            <div className='bg-[#171314] p-5  inset-shadow-sm rounded-2xl inset-shadow-zinc-500 hover:inset-shadow-red-500 hover:-translate-1 text-white font-bold text-xl'>C</div>
+            <div className='bg-[#171314] p-5  inset-shadow-sm  rounded-2xl inset-shadow-zinc-500 hover:inset-shadow-red-500 hover:-translate-1 text-white font-bold text-xl'>-</div>
+            <div className='bg-[#171314] p-5  inset-shadow-sm rounded-2xl inset-shadow-zinc-500 hover:inset-shadow-red-500 hover:-translate-1 text-white font-bold text-xl'>/</div>
+            <div className='bg-[#171314] p-5 inset-shadow-sm rounded-2xl inset-shadow-zinc-500 hover:inset-shadow-red-500 hover:-translate-1 text-white font-bold text-xl'>cl</div>
+
+          <div className='bg-[#bf0000] p-5  inset-shadow-lg rounded-2xl inset-shadow-zinc-100 hover:inset-shadow-red-50 hover:-translate-1 text-white font-bold text-xl'>7</div>
+<div className='bg-[#bf0000] p-5  inset-shadow-lg rounded-2xl inset-shadow-zinc-100 hover:inset-shadow-red-50 hover:-translate-1 text-white font-bold text-xl'>8</div>
+<div className='bg-[#bf0000] p-5  inset-shadow-lg rounded-2xl inset-shadow-zinc-100 hover:inset-shadow-red-50 hover:-translate-1 text-white font-bold text-xl'>9</div>
+<div className='bg-[#bf0000] p-5  inset-shadow-lg rounded-2xl inset-shadow-zinc-100 hover:inset-shadow-red-50 hover:-translate-1 text-white font-bold text-xl'>*</div>
+            
+            <div className='bg-[#bf0000] p-5  inset-shadow-lg rounded-2xl inset-shadow-zinc-100 hover:inset-shadow-red-50 hover:-translate-1 text-white font-bold text-xl'>4</div>
+<div className='bg-[#bf0000] p-5  inset-shadow-lg rounded-2xl inset-shadow-zinc-100 hover:inset-shadow-red-50 hover:-translate-1 text-white font-bold text-xl'>5</div>
+<div className='bg-[#bf0000] p-5  inset-shadow-lg rounded-2xl inset-shadow-zinc-100 hover:inset-shadow-red-50 hover:-translate-1 text-white font-bold text-xl'>6</div>
+<div className='bg-[#bf0000] p-5  inset-shadow-lg rounded-2xl inset-shadow-zinc-100 hover:inset-shadow-red-500 hover:-translate-1 text-white font-bold text-xl'>+</div>
+
+<div className='bg-[#bf0000] p-5  inset-shadow-lg rounded-2xl inset-shadow-zinc-100 hover:inset-shadow-red-50 hover:-translate-1 text-white font-bold text-xl'>1</div>
+<div className='bg-[#bf0000] p-5  inset-shadow-lg rounded-2xl inset-shadow-zinc-100 hover:inset-shadow-red-50 hover:-translate-1 text-white font-bold text-xl'>2</div>
+<div className='bg-[#bf0000] p-5  inset-shadow-lg rounded-2xl inset-shadow-zinc-100 hover:inset-shadow-red-50 hover:-translate-1 text-white font-bold text-xl'>3</div>
+<div className='bg-[#bf0000] p-5 row-span-2 inset-shadow-lg rounded-2xl inset-shadow-zinc-100 hover:inset-shadow-red-50 hover:-translate-1 text-white font-bold text-xl flex justify items-center'>=</div>
+
+            <div className='bg-[#bf0000] col-span-2 p-5  inset-shadow-lg rounded-2xl inset-shadow-zinc-100 hover:inset-shadow-red-50 hover:-translate-1 text-white font-bold text-xl flex justify-center'>0</div>
+            <div className='bg-[#bf0000] p-5  inset-shadow-lg rounded-2xl inset-shadow-zinc-100 hover:inset-shadow-red-50 hover:-translate-1 text-white font-bold text-xl'>.</div>
+            
+          </div>
+          </div>
+          
+       </div>
+</>
+     
   )
 }
 
